3ff912ae16badfa653706ce7f8b872be4f89af4f
lankeshwhatsapp4444 <151029357+lankeshwhatsapp4444@users.noreply.github.com>
2024-10-17 00:54:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=iug3yBwR#ZnpqwTK-f1i4W5JMGExVXgRnSzPhmLgHbSDXbY0R9Ik",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
